2a3e756bc61188619a21bc273c90e41d1fe674c3
Marcos Jesus <marcos.jesus@cliqx.com.br>
2023-08-08 06:03:48 -0300

    ed: Conjunto intersecion

--- conjuntos.js
+++ conjuntos.js
@@ -4,12 +4,15 @@ class Conjuntos {
   }
 
   has(element) {
+    debugger
     return Object.prototype.hasOwnProperty.call(this.items, element)
   }
 
+  
   add(element) {
     if(!this.has(element)) {
       this.items[element] = element
+      debugger
       return true
     }
   }
@@ -24,9 +27,40 @@ class Conjuntos {
 
   union(otherSet) {
     const unionSet = new Conjuntos()
-    this.values.forEach(value => unionSet.add(value))
+    debugger
+    this.values().forEach(value => unionSet.add(value))
+    debugger
     otherSet.values().forEach(value => unionSet.add(value))
+    debugger
     return unionSet
   }
 
+  intersectionUnion(otherSet) {
+    const intersection = new Conjuntos()
+    const values = this.values()
+    debugger
+
+    for (let i=0; i< values.length; i++) {
+      if (otherSet.has(values[i])) {
+        intersection.add(values[i])
+      }
+    }
+
+    debugger
+
+    return intersection
+  }
+
 }
+
+const setA = new Conjuntos()
+setA.add(1)
+setA.add(2)
+setA.add(3)
+const setB = new Conjuntos()
+setB.add(2)
+setB.add(3)
+setB.add(4)
+
+const intersectionAB = setA.intersectionUnion(setB)
+console.log(intersectionAB.values())
